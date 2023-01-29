8528e5bd8020893b2f560a53b16dad4fd70cda66
YeowonAhn <dudnjs@sookmyung.ac.kr>
2023-01-29 12:22:55 +0900

    yeowon second commit

---
+++ src/component/ui/likes.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 300px;
+  height: 20px;
+  flex-direction: row;
+  padding-top: 0px;
+  padding-right: 0px;
+  padding-bottom: 15px;
+  padding-left: 0px;
+  margin: 0 auto;
+  display: block;
+ 
+  .upicon{
+    color: #434343;
+    font-size: 15px;
+  }
+  .downicon{
+    color: #434343;
+    font-size: 15px;
+  }
+  .cmicon{
+    color: #434343;
+    font-size: 15px;
+  }
+  .staricon{
+    color: #434343;
+    font-size: 15px;
+  }
+  .count{
+    color: #434343;
+    font-weight: 500;
+    font-size: 15px;
+    line-height: 16px;
+    align-items: center;
+    text-align: center;
+  }
+`;
+const Space_1 = styled.div`
+  width: 7px;
+  height: auto;
+  display: inline-block;
+`;
+const Space_2 = styled.div`
+  width: 15px;
+  height: auto;
+  display: inline-block;
+`;
+
+function Likes() {
+  const [thumbup, setThumbUp] = useState(0); //좋아요 버튼구현
+  const [thumbdown, setThumbDown] = useState(0); //좋아요 버튼구현
+  const [msg, setMsg] = useState(0); //좋아요 버튼구현
+  const [star, setStar] = useState(0); //좋아요 버튼구현
+
+  return (
+    <Wrapper>
+      <span
+        onClick={() => {
+            setThumbUp(thumbup + 1);
+        }}
+      >
+        <i className="upicon fa-regular fa-thumbs-up"></i>
+      </span>
+      <Space_1/>
+      <i className="count">{thumbup}</i>
+     <Space_2/>
+     
+      <span
+        onClick={() => {
+            setThumbDown(thumbdown + 1);
+        }}
+      >
+        <i className="downicon fa-regular fa-thumbs-down"></i>
+      </span>
+      <Space_1/>
+      <i className="count">{thumbdown}</i>
+      <Space_2/>
+      
+      <span
+        onClick={() => {
+            setMsg(msg + 1);
+        }}
+      >
+        <i className="cmicon fa-regular fa-comment-dots"></i>
+      </span>
+      <Space_1/>
+      <i className="count">{msg}</i>
+      <Space_2/>
+      
+      <span
+        onClick={() => {
+            setStar(star + 1);
+        }}
+      >
+        <i className="staricon fa-regular fa-star"></i>
+      </span>
+      <Space_1/>
+      <i className="count">{star}</i>
+    </Wrapper>
+  );
+}
+
+export default Likes;
