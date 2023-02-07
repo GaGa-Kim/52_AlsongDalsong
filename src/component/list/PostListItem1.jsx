369fe4a41275eb94a33de3c0a4d09ae074253241
chae <jungchae5812@sookmyung.ac.kr>
2023-02-07 23:35:22 +0900

    merge 위한 재수정

---
+++ src/component/list/PostListItem1.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import styled from "styled-components";
+import HorizonLine from "../ui/HorizontalLine";
+import Likes from "../ui/likes";
+
+const Wrapper = styled.div`
+  width: calc(100% - 32px);
+
+  padding-top: 10px;
+  padding-right: 16px;
+  padding-bottom: 10px;
+  padding-left: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  border-color: transparent;
+  border-radius: 20px;
+  cursor: pointer;
+  background: #efefef;
+  :hover {
+    background: lightgrey;
+  }
+  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.25);
+
+  .DecisionIcon {
+    font-size: 30px;
+    position: absolute;
+    margin-left: 250px;
+    margin-top: -120px;
+    color: #fa0050;
+  }
+`;
+const NameandDay = styled.p`
+  position: absolute;
+  width: 280px;
+  height: 40px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 13px;
+  line-height: 16px;
+`;
+
+const TitleText = styled.p`
+  width: 280px;
+  height: 5px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 17px;
+  font-family: 'GmarketSansTTFMedium';
+  text-align: left;
+  padding-top: 10px;
+  color: #000000;
+  
+`;
+const ContentText = styled.p`
+  white-space: pre-wrap;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  font-family: 'GmarketSansTTFMedium';
+  line-height: 15px;
+  display: flex;
+  text-align: left;
+  padding-bottom: 20px;
+  padding-top: 30px;
+  color: #000000;
+`;
+const Space = styled.div`
+  width: 15px;
+  height: auto;
+  display: inline-block;
+`;
+
+function PostListItem(props) {
+  const { post, onClick } = props;
+
+  return (
+    <Wrapper onClick={onClick}>
+      <NameandDay></NameandDay>
+      <TitleText>{post.title}</TitleText>
+      <i className="DecisionIcon fa-regular fa-circle-check"></i>
+      <ContentText>{post.content}</ContentText>
+      <HorizonLine />
+      <Likes />
+    </Wrapper>
+  );
+}
+
+export default PostListItem;
